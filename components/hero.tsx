a21ca72df2f95c16862592b2b2c1c095bd507df4
mwangimuringi <mwangimuringi10@gmail.com>
2024-08-10 20:43:51 +0300

    ...

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-      Hero
+   <div className="hero">
+    <div className="flex-1 pt-36 px-0">
+      <h1 className='hero__title'>
+        Find, book or rent a car - quickly and easily !
+      </h1>
+      <p className='hero__subtitle'>
+        Streamline your car rental experience with our effortless booking process.
+      </p>
     </div>
+   </div>
   )
 }
 

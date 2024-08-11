12e7ee929665038af9628d938c1e14cb9b1be7eb
mwangimuringi <mwangimuringi10@gmail.com>
2024-08-11 14:50:10 +0300

    completed the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,25 +1,59 @@
+import { footerLinks } from '@/constants'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
     <footer className='flex flex-col text-black mt-5 border-t bg-gray-100'>
-        <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
-            <div className="flex flex-col justify-start items-start gap-6">
-                <Image src={'/logo.svg'} 
-                alt={'logo'}
-                width={118} 
-                height={18} 
-                className='object-contain'/>
-                <p className='text-base text-gray-700'>Kenya Wheels 2023
-                  <br />All rights reserved &copy;
-                </p>
-            </div>
-            
-            <div className="footer__links">
-                
-            </div>
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src={'/logo.svg'}
+            alt={'logo'}
+            width={118}
+            height={18}
+            className='object-contain'
+          />
+          <p className='text-base text-gray-700'>
+            Kenya Wheels 2023
+            <br />
+            All rights reserved &copy;
+          </p>
         </div>
+
+        <div className="footer__links">
+          {footerLinks.map((item) => (
+            <div key={item.title} className='footer__link'>
+              <h3 className='font-bold'>{item.title}</h3>
+              <div className="flex flex-col gap-5">
+                {item.links.map((link) => (
+                  <Link
+                    key={link.title}
+                    href={link.url}
+                    className="text-gray-500"
+                  >
+                    {link.title}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
+        <p>@2024 Kenya Wheels. All rights reserved</p>
+
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy & Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms & Condition
+          </Link>
+        </div>
+      </div>
     </footer>
   )
 }

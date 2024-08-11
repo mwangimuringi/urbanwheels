809e47c5561377c6d3d90f7fa92408af6c7db8a4
mwangimuringi <mwangimuringi10@gmail.com>
2024-08-11 15:13:34 +0300

    ...

--- components/hero.tsx
+++ components/hero.tsx
@@ -17,25 +17,22 @@ const Hero = () => {
         Streamline your car rental experience with our effortless booking process.
       </p>
       <CustomButton
-      title = "Explore cars"
-      containerStyles = "bg-primary-blue text-white rounded-full mt-10"
-      handleClick = {handleScroll}
-      />
+          title="Explore cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll} btnType={'button'}      />
     </div>
         {/* image body */}
-    <div className="hero__image = container">
+    <div className="hero__image-container">
       <div className="hero__image">
         <Image 
-          // src="/pattern.png"
           src={`/hero.png`}
           alt="hero"
           fill
           className="object-contain"
         />
-        <div className="hero__image-overlay"/>
       </div>
+        <div className="hero__image-overlay"/>
     </div>
-
    </div>
   )
 }

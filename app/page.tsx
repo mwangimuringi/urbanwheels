a32dcb6b4ffb0fe82091c52f78d62b191d4531ed
mwangimuringi <mwangimuringi10@gmail.com>
2024-08-13 17:17:52 +0300

    Updated ShoMore button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { ShowMore } from '@/components';
 import CarCard from '@/components/CarCard';
 import CustomFilter from '@/components/CustomFilter';
 import Hero from '@/components/hero';
@@ -39,7 +40,10 @@ export default async function Home({searchParams}) {
               ))}
             </div>
 
-            
+            <ShowMore 
+             pageNumber={(searchParams.limit || 10) / 10}
+             isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className='home__error-container'>
--- types/index.ts
+++ types/index.ts
@@ -49,3 +49,8 @@ export interface OptionProps {
     options: OptionProps[];
   }
   
+  export interface ShowMoreProps {
+    pageNumber: number;
+    isNext: boolean;
+  }
+  

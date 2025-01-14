fe55e56993ecbfa14b786f53e0a640d95639baaa
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-14 22:54:57 +0300

    Refactor CarDetails component by extracting reusable components

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,114 +1,132 @@
 "use client";
 
-import { CarProps } from '@/types';
-import { Dialog, DialogPanel, Transition, TransitionChild } from '@headlessui/react';
-import React, { Fragment } from 'react'
-import Image from 'next/image'
-import { generateCarImageUrl } from '@/utilities';
-
+import { CarProps } from "@/types";
+import {
+  Dialog,
+  DialogPanel,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
+import React, { Fragment } from "react";
+import Image from "next/image";
+import { generateCarImageUrl } from "@/utilities";
 
 interface CarDetailsProps {
-    isOpen: boolean;
-    closeModal: () => void;
-    car: CarProps;
+  isOpen: boolean;
+  closeModal: () => void;
+  car: CarProps;
 }
+
+const CarImage = ({ src, alt }: { src: string; alt: string }) => (
+  <div className="relative w-full h-24 bg-primary-blue-100 rounded-lg">
+    <Image src={src} alt={alt} fill priority className="object-contain" />
+  </div>
+);
+
+const CarInfoItem = ({
+  label,
+  value,
+}: {
+  label: string;
+  value: string | number;
+}) => (
+  <div className="flex justify-between gap-5 w-full text-right">
+    <h4 className="text-grey capitalize">{label}</h4>
+    <p className="text-black-100 font-semibold">{value}</p>
+  </div>
+);
 
 const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
-    return (
-        <>
-            <Transition appear show={isOpen} as={Fragment}>
-                <Dialog as='div' className='relative z-10' onClose={closeModal}>
-                    <TransitionChild
-                        as={Fragment}
-                        enter='ease-out duration-300'
-                        enterFrom='opacity-0'
-                        enterTo='opacity-100'
-                        leave='ease-in duration-200'
-                        leaveFrom='opacity-100'
-                        leaveTo='opacity-0'
-                    >
-                        <div className='fixed inset-0 bg-black bg-opacity-25' />
-                    </TransitionChild>
+  if (!car) return null;
 
-                    <div className="fixed inset-0 overflow-y-auto">
-                        <div className="flex min-h-full items-center justify-center p-4 text-center">
-                            <TransitionChild
-                                as={Fragment}
-                                enter='ease-out duration-300'
-                                enterFrom='opacity-0 scale-95'
-                                enterTo='opacity-100 scale-100'
-                                leave='ease-out duration-300'
-                                leaveFrom='opacity-100 scale-100'
-                                leaveTo='opacity-0 scale-95'
-                            >
-                                <DialogPanel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
-                                    <button
-                                        type='button'
-                                        className='absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full'
-                                        onClick={closeModal}
-                                    >
-                                        <Image
-                                            src={'/close.svg'}
-                                            alt='close'
-                                            width={20}
-                                            height={20}
-                                            className='object-contain'
-                                        />
-                                    </button>
+  const carInfo = Object.entries(car);
 
-                                    <div className="flex-1 flex flex-col gap-3">
-                                        <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
-                                            <Image
-                                                src={`/hero.png`} alt="car model" fill priority className='object-contain'
-                                            // src={generateCarImageUrl(car)} alt="car model" fill priority className='object-contain'
-                                            />
-                                        </div>
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <TransitionChild
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </TransitionChild>
 
-                                        <div className='flex gap-3'>
-                                            <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                                                <Image
-                                                    src={generateCarImageUrl(car, '29')} alt="car model" fill priority className='object-contain'
-                                                />
-                                            </div>
-                                            <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                                                <Image
-                                                    src={generateCarImageUrl(car, '33')} alt="car model" fill priority className='object-contain'
-                                                />
-                                            </div>
-                                            <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                                                <Image
-                                                    src={generateCarImageUrl(car, '27')} alt="car model" fill priority className='object-contain'
-                                                />
-                                            </div>
-                                        </div>
-                                    </div>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <TransitionChild
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-out duration-300"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <DialogPanel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
+                <button
+                  type="button"
+                  className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
+                  onClick={closeModal}
+                  aria-label="Close details"
+                >
+                  <Image
+                    src={"/close.svg"}
+                    alt="Close"
+                    width={20}
+                    height={20}
+                    className="object-contain"
+                  />
+                </button>
 
-                                    <div className='flex-1 flex flex-col gap-2'>
-                                        <h2 className='font-semibold text-xl capitalize'>
-                                            {car.make} {car.model}
-                                        </h2>
+                <div className="flex-1 flex flex-col gap-3">
+                  <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
+                    <Image
+                      src={"/hero.png"}
+                      alt="Car model"
+                      fill
+                      priority
+                      className="object-contain"
+                    />
+                  </div>
 
-                                        <div className='mt-3 flex flex-wrap gap-4'>
-                                            {Object.entries(car).map(([key, value]) => (
-                                                <div className='flex justify-between gap-5 w-full text-right' key={key} >
-                                                    <h4 className='text-grey capitalize'>
-                                                        {key.split("_").join(" ")}
-                                                    </h4>
-                                                    <p className='text-black-100 font-semibold'>
-                                                        {value}
-                                                    </p>
-                                                </div>
-                                            ))}
-                                        </div>
-                                    </div>
-                                </DialogPanel>
-                            </TransitionChild>
-                        </div>
-                    </div>
-                </Dialog>
-            </Transition>
-        </>
-    )
-}
+                  <div className="flex gap-3">
+                    {["29", "33", "27"].map((angle) => (
+                      <CarImage
+                        key={angle}
+                        src={generateCarImageUrl(car, angle)}
+                        alt={`Car view ${angle}`}
+                      />
+                    ))}
+                  </div>
+                </div>
 
-export default CarDetails
+                <div className="flex-1 flex flex-col gap-2">
+                  <h2 className="font-semibold text-xl capitalize">
+                    {car.make} {car.model}
+                  </h2>
+
+                  <div className="mt-3 flex flex-wrap gap-4">
+                    {carInfo.map(([key, value]) => (
+                      <CarInfoItem
+                        key={key}
+                        label={key.split("_").join(" ")}
+                        value={value}
+                      />
+                    ))}
+                  </div>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
+
+export default CarDetails;

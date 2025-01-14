ea4c6a652101fd2d7007cfa867f2f446d2685009
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-14 23:11:04 +0300

    Reduced redundancy by using constants for frequently reused class names.

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -11,24 +11,19 @@ import React, { Fragment } from "react";
 import Image from "next/image";
 import { generateCarImageUrl } from "@/utilities";
 
-interface CarProps {
-    make: string;
-    model: string;
-    year: number;
-    engineType?: string;
-    color?: string;
-    [key: string]: string | number | undefined;
-  }
-  
+interface CarDetailsProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  car: CarProps;
+}
 
-  const commonImageClass = "relative w-full h-24 bg-primary-blue-100 rounded-lg";
+const commonImageClass = "relative w-full h-24 bg-primary-blue-100 rounded-lg";
 
-  const CarImage = ({ src, alt }: { src: string; alt: string }) => (
-    <div className={commonImageClass}>
-      <Image src={src} alt={alt} fill priority className="object-contain" />
-    </div>
-  );
-  
+const CarImage = ({ src, alt }: { src: string; alt: string }) => (
+  <div className={commonImageClass}>
+    <Image src={src} alt={alt} fill priority className="object-contain" />
+  </div>
+);
 
 const CarInfoItem = ({
   label,
@@ -75,61 +70,67 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
               leaveTo="opacity-0 scale-95"
             >
               <DialogPanel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
-              <button
-  type="button"
-  className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
-  onClick={closeModal}
-  aria-label="Close details"
-  title="Close details"
->
-  <Image
-    src={"/close.svg"}
-    alt="Close"
-    width={20}
-    height={20}
-    className="object-contain"
-  />
-</button>
+                <button
+                  type="button"
+                  className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
+                  onClick={closeModal}
+                  aria-label="Close details"
+                  title="Close details"
+                >
+                  <Image
+                    src={"/close.svg"}
+                    alt="Close"
+                    width={20}
+                    height={20}
+                    className="object-contain"
+                  />
+                </button>
 
-<Image
-  src={"/hero.png"}
-  alt="Primary car model image"
-  fill
-  priority
-  className="object-contain"
-/>
-
-
-                  <div className="flex gap-3">
-                    {["29", "33", "27"].map((angle) => (
-                      <CarImage
-                        key={angle}
-                        src={generateCarImageUrl(car, angle)}
-                        alt={`Car view ${angle}`}
-                      />
-                    ))}
-                  </div>
-                </div>
-
-                <div className="flex-1 flex flex-col gap-2">
+                <div className="flex-1 flex flex-col gap-3">
                   {car.make && car.model && (
-                    <h2 className="font-semibold text-xl capitalize">
-                      {car.make} {car.model}
-                    </h2>
+                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
+                      <Image
+                        src={"/hero.png"}
+                        alt="Primary car model image"
+                        fill
+                        priority
+                        className="object-contain"
+                      />
+                    </div>
                   )}
 
-                  {carInfo.length > 0 && (
-                    <div className="mt-3 flex flex-wrap gap-4">
-                      {carInfo.map(([key, value], index) => (
-                        <CarInfoItem
-                          key={`car-info-${index}`}
-                          label={key.split("_").join(" ")}
-                          value={value}
+                  {car.make && car.model && (
+                    <div className="flex gap-3">
+                      {["29", "33", "27"].map((angle) => (
+                        <CarImage
+                          key={angle}
+                          src={generateCarImageUrl(car, angle)}
+                          alt={`Car view ${angle}`}
                         />
                       ))}
                     </div>
                   )}
                 </div>
+
+                {car.make && car.model && (
+                  <div className="flex-1 flex flex-col gap-2">
+                    <h2 className="font-semibold text-xl capitalize">
+                      {car.make} {car.model}
+                    </h2>
+
+                    {carInfo.length > 0 && (
+                      <div className="mt-3 flex flex-wrap gap-4">
+                        {carInfo.map(([key, value], index) => (
+                          <CarInfoItem
+                            key={`car-info-${index}`}
+                            label={key.split("_").join(" ")}
+                            value={value}
+                          />
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                )}
               </DialogPanel>
             </TransitionChild>
           </div>

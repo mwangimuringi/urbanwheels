3fe5f45d42494619bd9d183e8536ba215fa8d28a
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-14 23:18:31 +0300

    Fixed button disabling behavior using isDisabled prop.

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,31 +1,32 @@
-'use client'
-import { CustomButtonProps } from '@/types'
-import React from 'react'
+"use client";
+import { CustomButtonProps } from "@/types";
+import React from "react";
 import Image from "next/image";
 
-const CustomButton: React.FC<CustomButtonProps> = ({ title, containerStyles, handleClick, btnType, textStyles, rightIcon, isDisabled }) => {
-    return (
-        <button
-            disabled={false}
-            type={"button"}
-            className={`custom-btn ${containerStyles}`}
-            onClick={handleClick}
-        >
-            <span className={`flex-1 ${textStyles}`}>
-                {title}
-            </span>
-            {rightIcon && (
-                <div className="relative w-6 h-6">
-                    <Image
-                        src={rightIcon}
-                        alt="arrow_left"
-                        fill
-                        className="object-contain"
-                    />
-                </div>
-            )}
-        </button>
-    )
-}
+const CustomButton: React.FC<CustomButtonProps> = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+  textStyles,
+  rightIcon,
+  isDisabled,
+}) => {
+  return (
+    <button
+      disabled={isDisabled} // Updated to use isDisabled prop
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image src={rightIcon} alt="icon" fill className="object-contain" />
+        </div>
+      )}
+    </button>
+  );
+};
 
-export default CustomButton
+export default CustomButton;

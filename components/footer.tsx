4e93b054418ed6daa254802c25c000e054ada0c6
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-15 21:39:06 +0300

    Improve screen reader accessibility by enhancing alt text for images.

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,13 +8,14 @@ const Footer = () => {
     <footer className="flex flex-col text-black mt-5 border-t bg-gray-100">
       <section className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
         <div className="flex flex-col justify-start items-start gap-6">
-          <Image
-            src={"/logo.svg"}
-            alt={"logo"}
-            width={118}
-            height={18}
-            className="object-contain"
-          />
+        <Image
+  src={'/logo.svg'}
+  alt={'Kenya Wheels company logo'}
+  width={118}
+  height={18}
+  className='object-contain'
+/>
+
           <p className="text-base text-gray-700">
             Kenya Wheels 2023
             <br />

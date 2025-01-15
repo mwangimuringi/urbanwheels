efb31b517dd25644b5fd5fab5b24a79dc2bc8bc4
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-15 21:40:12 +0300

    Added dynamic copyright year to Footer.

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,20 +4,21 @@ import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="flex flex-col text-black mt-5 border-t bg-gray-100">
       <section className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
         <div className="flex flex-col justify-start items-start gap-6">
-        <Image
-  src={'/logo.svg'}
-  alt={'Kenya Wheels company logo'}
-  width={118}
-  height={18}
-  className='object-contain'
-/>
-
+          <Image
+            src={"/logo.svg"}
+            alt={"Kenya Wheels company logo"}
+            width={118}
+            height={18}
+            className="object-contain"
+          />
           <p className="text-base text-gray-700">
-            Kenya Wheels 2023
+            Kenya Wheels {currentYear}
             <br />
             All rights reserved &copy;
           </p>
@@ -44,7 +45,7 @@ const Footer = () => {
       </section>
 
       <section className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
-        <p>@2024 Kenya Wheels. All rights reserved</p>
+        <p>@{currentYear} Kenya Wheels. All rights reserved</p>
 
         <nav className="footer__copyrights-link">
           <Link href="/" className="text-gray-500">

5bb7f0d4a581703dcd7de16cda36a1ee76b8f6fe
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-15 21:38:03 +0300

    Use semantic HTML elements for accessibility in Footer.

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,31 +1,31 @@
-import { footerLinks } from '@/constants'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className='flex flex-col text-black mt-5 border-t bg-gray-100'>
-      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+    <footer className="flex flex-col text-black mt-5 border-t bg-gray-100">
+      <section className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
         <div className="flex flex-col justify-start items-start gap-6">
           <Image
-            src={'/logo.svg'}
-            alt={'logo'}
+            src={"/logo.svg"}
+            alt={"logo"}
             width={118}
             height={18}
-            className='object-contain'
+            className="object-contain"
           />
-          <p className='text-base text-gray-700'>
+          <p className="text-base text-gray-700">
             Kenya Wheels 2023
             <br />
             All rights reserved &copy;
           </p>
         </div>
 
-        <div className="footer__links">
+        <nav className="footer__links">
           {footerLinks.map((item) => (
-            <div key={item.title} className='footer__link'>
-              <h3 className='font-bold'>{item.title}</h3>
+            <div key={item.title} className="footer__link">
+              <h3 className="font-bold">{item.title}</h3>
               <div className="flex flex-col gap-5">
                 {item.links.map((link) => (
                   <Link
@@ -39,23 +39,23 @@ const Footer = () => {
               </div>
             </div>
           ))}
-        </div>
-      </div>
+        </nav>
+      </section>
 
-      <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
+      <section className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
         <p>@2024 Kenya Wheels. All rights reserved</p>
 
-        <div className="footer__copyrights-link">
+        <nav className="footer__copyrights-link">
           <Link href="/" className="text-gray-500">
             Privacy & Policy
           </Link>
           <Link href="/" className="text-gray-500">
             Terms & Condition
           </Link>
-        </div>
-      </div>
+        </nav>
+      </section>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;

fc0d9ec92b361a43932f0a3f7fabe2f57146ea6c
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-16 23:05:38 +0300

    Added accessibity.

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { ShowMoreProps } from "@/types";
 import { useRouter } from "next/navigation";
@@ -6,29 +6,25 @@ import CustomButton from "./CustomButton";
 import { updateSearchParams } from "@/utilities";
 
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleNavigation = () => {
-        // Calculate the new limit based on the page number and navigation type
-        const newLimit = (pageNumber + 1) * 10;
+  const handleNavigation = () => {
+    // Calculate the new limit based on the page number and navigation type
+    const newLimit = (pageNumber + 1) * 10;
 
-        // Update the "limit" search parameter in the URL with the new value
-        const newPathname = updateSearchParams("limit", `${newLimit}`);
+    // Update the "limit" search parameter in the URL with the new value
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
 
-        router.push(newPathname);
-    };
+    router.push(newPathname);
+  };
 
-    return (
-        <div className="w-full flex-center gap-5 mt-10">
-            {!isNext && (
-                <CustomButton
-                    btnType="button"
-                    title="Show More"
-                    containerStyles="bg-primary-blue rounded-full text-white"
-                    handleClick={handleNavigation} textStyles={""} isDisabled={false} />
-            )}
-        </div>
-    )
-}
+  return (
+    <div className="w-full flex justify-center items-center gap-5 mt-10">
+      {!isNext && (
+        
+      )}
+    </div>
+  );
+};
 
-export default ShowMore
+export default ShowMore;

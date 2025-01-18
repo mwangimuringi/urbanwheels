5960ec44150dd4cbfb129f68dcb24dc804beab33
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-18 22:33:48 +0300

    Enhanced type-safety.

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -5,6 +5,11 @@ import { useRouter } from "next/navigation";
 import CustomButton from "./CustomButton";
 import { updateSearchParams } from "@/utilities";
 
+export interface ShowMoreProps {
+  pageNumber: number; // Ensure pageNumber is a non-negative integer
+  isNext: boolean;    // Define isNext explicitly as a boolean
+}
+
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const router = useRouter();
 
@@ -17,7 +22,7 @@ const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
       }
       router.push(newPathname);
     } catch (error) {
-      console.error("Navigation Error:", error);
+      console.error("Navigation Error:", error.message);
     }
   };
 

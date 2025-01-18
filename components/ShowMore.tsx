9d046b7e85fff42c6a342408bd25b94cdd72bc0f
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-18 22:29:23 +0300

    improved styling

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -5,21 +5,13 @@ import { useRouter } from "next/navigation";
 import CustomButton from "./CustomButton";
 import { updateSearchParams } from "@/utilities";
 
-/**
- * ShowMore Component
- * Displays a button to load more results by updating the limit in the URL.
- * @param {ShowMoreProps} props - Component properties including the current page number and navigation status.
- */
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const router = useRouter();
 
-  /**
-   * Handle navigation to update the URL limit and fetch the next set of results.
-   */
   const handleNavigation = () => {
-    const newLimit = (pageNumber + 1) * 10; // Calculate the new limit
-    const newPathname = updateSearchParams("limit", `${newLimit}`); // Update URL params
-    router.push(newPathname); // Navigate to the updated URL
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
+    router.push(newPathname);
   };
 
   return (
@@ -28,7 +20,7 @@ const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
         <CustomButton
           btnType="button"
           title="Show More"
-          containerStyles="bg-primary-blue rounded-full text-white px-4 py-2 hover:bg-primary-blue-dark focus:ring-2 focus:ring-offset-2 focus:ring-primary-blue-dark"
+          containerStyles="bg-primary-blue text-white rounded-full px-4 py-2 hover:bg-primary-blue-dark focus:ring focus:ring-offset-2 focus:ring-primary-blue-dark transition ease-in-out duration-200"
           handleClick={handleNavigation}
           textStyles="font-medium"
           isDisabled={false}

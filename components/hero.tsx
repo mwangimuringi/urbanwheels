5748d78bf19a91c0decb78f0ae7113976dc00ec8
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-18 22:41:16 +0300

    Extracted strings to constants for reusablity

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,6 +4,9 @@ import React from 'react';
 import CustomButton from './CustomButton';
 import Image from 'next/image';
 
+const HERO_TITLE = "Find, book, or rent a car – quickly and easily!";
+const HERO_SUBTITLE = "Streamline your car rental experience with our effortless booking process.";
+
 const Hero = () => {
   const handleScroll = () => {
     console.log("Scroll to explore section.");
@@ -12,12 +15,8 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="flex-1 pt-36 px-0">
-        <h1 className="hero__title">
-          Find, book, or rent a car – quickly and easily!
-        </h1>
-        <p className="hero__subtitle">
-          Streamline your car rental experience with our effortless booking process.
-        </p>
+        <h1 className="hero__title">{HERO_TITLE}</h1>
+        <p className="hero__subtitle">{HERO_SUBTITLE}</p>
         <CustomButton
           title="Explore cars"
           containerStyles="bg-primary-blue text-white rounded-full mt-10 hover:bg-primary-blue-dark transition ease-in-out"

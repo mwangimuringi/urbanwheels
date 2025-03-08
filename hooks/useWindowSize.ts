1fe3d1ff898eee629102de8a16e388733d910bc5
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-08 23:50:33 +0300

    Added event listener for window resize in useWindowSize.

--- hooks/useWindowSize.ts
+++ hooks/useWindowSize.ts
@@ -1,10 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const useWindowSize = () => {
-  const [windowSize, setWindowSize] = useState<{ width: number; height: number }>({
+  const [windowSize, setWindowSize] = useState<{
+    width: number;
+    height: number;
+  }>({
     width: window.innerWidth,
     height: window.innerHeight,
   });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return windowSize;
 };

ab9affd35caa0a87202bba88a84ca7772fafc573
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-08 23:56:19 +0300

    Initialized useLocalStorage hook.

---
+++ hooks/useLocalStorage.ts
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error("Error reading from localStorage", error);
+      return initialValue;
+    }
+  });
+
+  return [storedValue, setStoredValue] as const;
+};
+
+export default useLocalStorage;

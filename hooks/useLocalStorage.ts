d21c48c791907468208f4e2d966be5539bc2399c
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-08 23:57:55 +0300

    Added custom hook options for storage serialization and deserialization.

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
-const useLocalStorage = <T>(key: string, initialValue: T) => {
+const useLocalStorage = <T>(
+  key: string,
+  initialValue: T,
+  serialize: (value: T) => string = JSON.stringify,
+  deserialize: (value: string) => T = JSON.parse
+) => {
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
       const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
+      return item ? deserialize(item) : initialValue;
     } catch (error) {
       console.error("Error reading from localStorage", error);
       return initialValue;
@@ -14,7 +19,7 @@ const useLocalStorage = <T>(key: string, initialValue: T) => {
   const setValue = (value: T) => {
     try {
       setStoredValue(value);
-      window.localStorage.setItem(key, JSON.stringify(value));
+      window.localStorage.setItem(key, serialize(value));
     } catch (error) {
       console.error("Error writing to localStorage", error);
     }

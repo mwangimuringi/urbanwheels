8913c63d3aade259f9272ec2725b94604a4b7a10
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-08 23:53:03 +0300

    Added initial window size fallback for SSR (Server Side Rendering).

--- hooks/useWindowSize.ts
+++ hooks/useWindowSize.ts
@@ -7,27 +7,29 @@ const useWindowSize = (
     width: number;
     height: number;
   }>({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: typeof window !== "undefined" ? window.innerWidth : 0,
+    height: typeof window !== "undefined" ? window.innerHeight : 0,
   });
 
   useEffect(() => {
-    let timeoutId: NodeJS.Timeout;
-    const handleResize = () => {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => {
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        });
-      }, 200);
-    };
+    if (typeof window !== "undefined") {
+      let timeoutId: NodeJS.Timeout;
+      const handleResize = () => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+          setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+          });
+        }, 200);
+      };
 
-    window.addEventListener("resize", handleResize, options);
-    return () => {
-      clearTimeout(timeoutId);
-      window.removeEventListener("resize", handleResize, options);
-    };
+      window.addEventListener("resize", handleResize, options);
+      return () => {
+        clearTimeout(timeoutId);
+        window.removeEventListener("resize", handleResize, options);
+      };
+    }
   }, [options]);
 
   return windowSize;

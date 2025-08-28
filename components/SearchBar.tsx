7af994c3aafdd99747286115a0053b54eaf3e4c6
mwangimuringi <mwangimuringi10@gmail.com>
2025-08-28 14:51:46 +0300

    ...

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -97,8 +97,6 @@ const SearchBar = () => {
       </div>
       <div className="searchbar__item">
         image here
-
-        
       </div>
       <SearchButton otherClasses="max-sm:hidden" />
     </form>
